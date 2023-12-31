c5a4bdab5627e28a9894c1895c80e29cac2594d5
Diva Amanda <diva.amandaptr@gmail.com>
2023-12-31 22:34:05 +0700

    get started

---
+++ Spotify Clone/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Spotify Clone
+//
+//  Created by Diva Amanda Putri on 31/12/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Home"
+        view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Spotify Clone/Controllers/Core/TabBarViewController.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarViewController.swift
+//  Spotify Clone
+//
+//  Created by Diva Amanda Putri on 31/12/23.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+}
